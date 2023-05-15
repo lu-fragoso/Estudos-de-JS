2e1eda4faf3c7c84df079e69a6c578821fec51da
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-05-14 21:57:26 -0300

    Adicionando arquivos de estudo JS

---
+++ Projetos - Seção 4/1_variaveis/script.js
@@ -0,0 +1,18 @@
+let nome = 'lucas'
+console.log(nome);
+console.log(`O nome é ${nome}`);
+
+let laranjas = 5;
+console.log(laranjas * laranjas);
+
+nome = 'liz';
+console.log(nome);
+
+laranjas = 5456;
+console.log(laranjas)
+
+laranjas = 'laranjas';
+console.log(laranjas);
+
+let um=1,dois=2, tres=3;
+console.log(um+dois+tres);
