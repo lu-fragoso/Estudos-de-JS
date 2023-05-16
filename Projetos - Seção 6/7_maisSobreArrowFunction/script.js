09bd7876e929ecf214c345813b1d2a91b5df96f0
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-05-16 18:26:35 -0300

    Estudos sobre JS

---
+++ Projetos - Seção 6/7_maisSobreArrowFunction/script.js
@@ -0,0 +1,13 @@
+const multiplicaPorDois = (x) =>{
+    return x * 2
+}
+
+console.log(multiplicaPorDois(9))
+
+
+//usada quando a função será simples
+//boas práticas
+
+const multiplicaPorTres = x => x*3
+
+console.log(multiplicaPorTres(5))
