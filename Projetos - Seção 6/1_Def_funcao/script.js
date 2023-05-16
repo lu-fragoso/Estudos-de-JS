babe97a27e8a185c649c7ac2cd136f0726af3d87
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-05-16 18:13:11 -0300

    Estudos de JS

---
+++ Projetos - Seção 6/1_Def_funcao/script.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole(){
+console.log('Hello Word')
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log('O numero é: ',num)
+    
+}
+
+imprimirUmNumero(1);
+imprimirUmNumero(11);
+imprimirUmNumero(10);
+imprimirUmNumero(100);
+
+const numeroAleatorio = function() {
+    console.log(Math.random())
+}
+numeroAleatorio();
+numeroAleatorio();
