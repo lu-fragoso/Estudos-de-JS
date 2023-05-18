baea454fcaabf98aae1e3f39509e4cea76ec5696
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-05-17 22:22:21 -0300

    Estudos de JS

---
+++ Projetos - Seção 6/9_arg_default/script.js
@@ -0,0 +1,12 @@
+function potencia(base,expo = 2) {
+    return Math.pow(base,expo)
+}
+//setando um valor fixo no expo caso o usuário não coloque
+
+console.log(potencia(2))
+
+console.log(potencia(2,4))
+
+console.log(potencia(2,5))
+
+console.log(potencia(9))
